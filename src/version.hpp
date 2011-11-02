84914cfc886bf757f9110ebc2c35f6b59158e6c3
Ben Becker <benjamin.becker@gmail.com>
2011-11-02 02:00:14 -0700

    incl guard

--- src/version.hpp
+++ src/version.hpp
@@ -1,1 +1,7 @@
+#ifndef VWATCHD_VERSION_HPP
+#define VWATCHD_VERSION_HPP
+
+// Basic def for now.  TODO: git hash.
 #define VWATCHD_VERSION_STRING "0.1"
+
+#endif  // end include guard VWATCHD_VERSION_HPP
